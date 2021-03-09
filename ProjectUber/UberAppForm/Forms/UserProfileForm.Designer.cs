83a797fa6c3173f9704d1af684782927d4f0f249
lastvoidtemplar <jed205@abv.bg>
2021-03-09 14:53:42 +0200

    UserProfileForm

--- ProjectUber/UberAppForm/Forms/UserProfileForm.Designer.cs
+++ ProjectUber/UberAppForm/Forms/UserProfileForm.Designer.cs
@@ -35,14 +35,15 @@ namespace UberAppForm.Forms
             this.DeleteButton = new System.Windows.Forms.Button();
             this.SaveButton = new System.Windows.Forms.Button();
             this.InsertButton = new System.Windows.Forms.Button();
-            this.UserIdTextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.PasswordTextBox = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.UsernameTextBox = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.BackButton = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
             // 
             // label4
@@ -63,35 +64,37 @@ namespace UberAppForm.Forms
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(658, 366);
+            this.dataGridView1.Size = new System.Drawing.Size(658, 512);
             this.dataGridView1.TabIndex = 47;
             // 
             // UpdateButton
             // 
             this.UpdateButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UpdateButton.Location = new System.Drawing.Point(127, 264);
+            this.UpdateButton.Location = new System.Drawing.Point(126, 425);
             this.UpdateButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.UpdateButton.Name = "UpdateButton";
             this.UpdateButton.Size = new System.Drawing.Size(93, 32);
             this.UpdateButton.TabIndex = 46;
             this.UpdateButton.Text = "Update";
             this.UpdateButton.UseVisualStyleBackColor = true;
+            this.UpdateButton.Click += new System.EventHandler(this.UpdateButton_Click);
             // 
             // DeleteButton
             // 
             this.DeleteButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.DeleteButton.Location = new System.Drawing.Point(236, 264);
+            this.DeleteButton.Location = new System.Drawing.Point(236, 425);
             this.DeleteButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.DeleteButton.Name = "DeleteButton";
             this.DeleteButton.Size = new System.Drawing.Size(93, 32);
             this.DeleteButton.TabIndex = 45;
             this.DeleteButton.Text = "Delete";
             this.DeleteButton.UseVisualStyleBackColor = true;
+            this.DeleteButton.Click += new System.EventHandler(this.DeleteButton_Click);
             // 
             // SaveButton
             // 
             this.SaveButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.SaveButton.Location = new System.Drawing.Point(127, 264);
+            this.SaveButton.Location = new System.Drawing.Point(127, 425);
             this.SaveButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.SaveButton.Name = "SaveButton";
             this.SaveButton.Size = new System.Drawing.Size(93, 32);
@@ -99,26 +102,19 @@ namespace UberAppForm.Forms
             this.SaveButton.Text = "Save";
             this.SaveButton.UseVisualStyleBackColor = true;
             this.SaveButton.Visible = false;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
             // 
             // InsertButton
             // 
             this.InsertButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.InsertButton.Location = new System.Drawing.Point(22, 264);
+            this.InsertButton.Location = new System.Drawing.Point(22, 425);
             this.InsertButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.InsertButton.Name = "InsertButton";
             this.InsertButton.Size = new System.Drawing.Size(93, 32);
             this.InsertButton.TabIndex = 43;
             this.InsertButton.Text = "Insert";
             this.InsertButton.UseVisualStyleBackColor = true;
-            // 
-            // UserIdTextBox
-            // 
-            this.UserIdTextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.UserIdTextBox.Location = new System.Drawing.Point(110, 195);
-            this.UserIdTextBox.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.UserIdTextBox.Name = "UserIdTextBox";
-            this.UserIdTextBox.Size = new System.Drawing.Size(196, 28);
-            this.UserIdTextBox.TabIndex = 42;
+            this.InsertButton.Click += new System.EventHandler(this.InsertButton_Click);
             // 
             // label3
             // 
@@ -126,9 +122,9 @@ namespace UberAppForm.Forms
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(18, 198);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(74, 24);
+            this.label3.Size = new System.Drawing.Size(54, 24);
             this.label3.TabIndex = 41;
-            this.label3.Text = "User id:";
+            this.label3.Text = "User:";
             // 
             // PasswordTextBox
             // 
@@ -171,25 +167,37 @@ namespace UberAppForm.Forms
             // BackButton
             // 
             this.BackButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.BackButton.Location = new System.Drawing.Point(22, 335);
+            this.BackButton.Location = new System.Drawing.Point(22, 496);
             this.BackButton.Name = "BackButton";
             this.BackButton.Size = new System.Drawing.Size(109, 32);
             this.BackButton.TabIndex = 36;
             this.BackButton.Text = "Back";
             this.BackButton.UseVisualStyleBackColor = true;
+            this.BackButton.Click += new System.EventHandler(this.BackButton_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(22, 226);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(336, 191);
+            this.dataGridView2.TabIndex = 49;
             // 
             // UserProfileForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1069, 398);
+            this.ClientSize = new System.Drawing.Size(1069, 553);
+            this.Controls.Add(this.dataGridView2);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.UpdateButton);
             this.Controls.Add(this.DeleteButton);
             this.Controls.Add(this.SaveButton);
             this.Controls.Add(this.InsertButton);
-            this.Controls.Add(this.UserIdTextBox);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.PasswordTextBox);
             this.Controls.Add(this.label2);
@@ -198,7 +206,9 @@ namespace UberAppForm.Forms
             this.Controls.Add(this.BackButton);
             this.Name = "UserProfileForm";
             this.Text = "UserProfileForm";
+            this.Load += new System.EventHandler(this.UserProfileForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -212,12 +222,12 @@ namespace UberAppForm.Forms
         private System.Windows.Forms.Button DeleteButton;
         private System.Windows.Forms.Button SaveButton;
         private System.Windows.Forms.Button InsertButton;
-        private System.Windows.Forms.TextBox UserIdTextBox;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox PasswordTextBox;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox UsernameTextBox;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button BackButton;
+        private System.Windows.Forms.DataGridView dataGridView2;
     }
 }
--- ProjectUber/UberAppForm/Forms/UserProfileForm.cs
+++ ProjectUber/UberAppForm/Forms/UserProfileForm.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Business;
+using Data.Models;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,158 @@ namespace UberAppForm.Forms
 {
     public partial class UserProfileForm : Form
     {
+        private UserProfileBusiness userProfileBusiness = new UserProfileBusiness();
+        private UserBusiness userBusiness = new UserBusiness();
+        private int editid = 0;
+        private int selectedUsert = 0;
         public UserProfileForm()
         {
             InitializeComponent();
+        }
+
+        public MainForm main;
+
+        private void UserProfileForm_Load(object sender, EventArgs e)
+        {
+            UpdateGrid();
+            UpdateUserGrid();
+            ClearTextBoxes();
+        }
+
+        private void UpdateGrid()
+        {
+            dataGridView1.DataSource = userProfileBusiness.GetAll();
+            dataGridView1.ReadOnly = true;
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+        private void UpdateUserGrid()
+        {        
+            dataGridView2.DataSource = userBusiness.GetAll();
+            dataGridView2.ReadOnly = true;
+            dataGridView2.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+        private void ClearTextBoxes()
+        {
+            UsernameTextBox.Text = "";
+            PasswordTextBox.Text = "";         
+        }
+        private int GetIdOfSelectedUser()
+        {
+            if (dataGridView2.SelectedRows.Count > 0)
+            {
+                var item = dataGridView2.SelectedRows[0].Cells;
+                var id = int.Parse(item[0].Value.ToString());
+                return id;
+            }
+            return userBusiness.GetAll().First().Id;
+        }
+        private void InsertButton_Click(object sender, EventArgs e)
+        {
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+            int user_id = GetIdOfSelectedUser();
+            UserProfile userProfile = new UserProfile();
+            userProfile.Username = username;
+            userProfile.Password = password;
+            userProfile.UserId = user_id;
+            userProfileBusiness.Add(userProfile);
+            UpdateGrid();
+            ClearTextBoxes();
+            dataGridView2.ClearSelection();
+        }
+
+        private void UpdateTextBoxes(int id)
+        {
+            UserProfile userProfile = userProfileBusiness.Get(id);
+            UsernameTextBox.Text = userProfile.Username;
+            PasswordTextBox.Text = userProfile.Password;
+            dataGridView2.ClearSelection();
+            selectedUsert = userProfile.UserId;
+        }
+
+        private void ToggleSaveUpdate()
+        {
+            if (UpdateButton.Visible)
+            {
+                UpdateButton.Visible = false;
+                SaveButton.Visible = true;
+            }
+            else
+            {
+                UpdateButton.Visible = true;
+                SaveButton.Visible = false;
+            }
+        }
+
+        private void UpdateButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var item = dataGridView1.SelectedRows[0].Cells;
+                var id = int.Parse(item[0].Value.ToString());
+                UpdateTextBoxes(id);
+                ToggleSaveUpdate();
+                DisableSelect();
+                editid = id;
+            }
+        }
+
+        private void DisableSelect()
+        {
+            dataGridView1.Enabled = false;
+        }
+
+        private UserProfile GetEditUserProfile()
+        {
+            UserProfile userProfile = new UserProfile();
+            userProfile.Id = editid;
+            string username = UsernameTextBox.Text;
+            string password = PasswordTextBox.Text;
+            if (dataGridView2.SelectedRows.Count > 0)
+            {
+                var item = dataGridView1.SelectedRows[0].Cells;
+                selectedUsert = int.Parse(item[0].Value.ToString());
+            }
+            userProfile.Username = username; ;
+            userProfile.Password = password;
+            userProfile.UserId = selectedUsert;
+            return userProfile;
+        }
+
+        private void ResetSelect()
+        {
+
+            dataGridView1.ClearSelection();
+            dataGridView1.Enabled = true;
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            UserProfile editUserProfile = GetEditUserProfile();
+            userProfileBusiness.Update(editUserProfile);
+            UpdateGrid();
+            ResetSelect();
+            ToggleSaveUpdate();
+            ClearTextBoxes();
+            dataGridView2.ClearSelection();
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                var item = dataGridView1.SelectedRows[0].Cells;
+                var id = int.Parse(item[0].Value.ToString());
+                userProfileBusiness.Delete(id);
+                UpdateGrid();
+                ResetSelect();
+            }
+        }
+
+        private void BackButton_Click(object sender, EventArgs e)
+        {
+            main.Show();
+            this.Close();
         }
     }
 }
