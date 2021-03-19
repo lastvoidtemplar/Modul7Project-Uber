c7eedce56b4dfca5262fa3e397d8300d92923eae
lastvoidtemplar <jed205@abv.bg>
2021-03-19 14:02:32 +0200

    Insert Migration

---
+++ ProjectUber/Data/Migrations/202103190813049_Insert.cs
@@ -0,0 +1,22 @@
+﻿namespace Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Insert : DbMigration
+    {
+        public override void Up()
+        {
+            Sql(("INSERT INTO Towns(name,country,zipcode) Values('Haskovo','Bulgaria',6300)"));
+            Sql(("INSERT INTO Towns(name,country,zipcode) Values('Dimitrovgrad','Bulgaria',6400)"));
+            Sql(("INSERT INTO Towns(name,country,zipcode) Values('Sofia','Bulgaria',1000)"));
+            Sql(("INSERT INTO Towns(name,country,zipcode) Values('Berlin','German',10000)"));
+            Sql(("INSERT INTO Towns(name,country,zipcode) Values('London','England',20000)"));
+
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
